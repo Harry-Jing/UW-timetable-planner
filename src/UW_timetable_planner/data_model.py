8b26bbf9ba1c5d331853716e4924c4a289e8973c
Gan <487457439@qq.com>
2023-11-17 21:58:11 +0800

    complete get_course()
    modify data_model

--- src/UW_timetable_planner/data_model.py
+++ src/UW_timetable_planner/data_model.py
@@ -1,20 +1,31 @@
+from datetime import time
+
 from pydantic import BaseModel
 
 
 class ClassTime(BaseModel):
-    start_time: str
-    end_time: str
+    start_time: time
+    end_time: time
     day: str
 
 
 class SubSection(BaseModel):
+    code: str
     times: list[ClassTime]
+    type: str
 
 
 class Lecture(BaseModel):
-    quiz: list[SubSection] = []
-    laboratory: list[SubSection] = []
+    code: str
+    quizzes: list[SubSection] = []
+    laboratories: list[SubSection] = []
     times: list[ClassTime]
+
+    def add_subsection(self, subsection):
+        if subsection.type == "quiz":
+            self.quizzes.append(subsection)
+        if subsection.type == "laboratory":
+            self.laboratories.append(subsection)
 
 
 class Course(BaseModel):
--- src/UW_timetable_planner/main.py
+++ src/UW_timetable_planner/main.py
@@ -1,6 +1,8 @@
-import httpx
+import json
+from datetime import datetime
 
-from .data_model import Course, Lecture
+import httpx
+from data_model import Course, Lecture, ClassTime, SubSection
 
 
 def get_course_details(course_code: str) -> dict:
@@ -10,13 +12,57 @@ def get_course_details(course_code: str) -> dict:
     return response.json()
 
 
+def get_course_details_local(course_code: str) -> dict:
+    with open(f"tests\\data\\{course_code}.json") as f:
+        return json.load(f)
+
+
 def get_course(course_code: str) -> Course:
-    data: dict = get_course_details(course_code)
+    data: dict = get_course_details_local(course_code)
     lectures = []
+    subsections = []
 
     sections = data["courseOfferingInstitutionList"][0]["courseOfferingTermList"][0][
         "activityOfferingItemList"
     ]
 
     for i in sections:
-        ...
+        code = i["code"]
+        times = []
+
+        for meetingDetail in i["meetingDetailsList"]:
+            time = meetingDetail["time"].split(" - ")
+            start_time = datetime.strptime(time[0], "%I:%M %p").time()
+            end_time = datetime.strptime(time[1], "%I:%M %p").time()
+            days_str = meetingDetail["days"]
+            days = []
+            for index in range(len(days_str)):
+                try:
+                    if days_str[index + 1].islower():
+                        days.append(days_str[index] + days_str[index + 1])
+                    elif days_str[index].isupper():
+                        days.append(days_str[index])
+                except IndexError:
+                    if days_str[index].isupper():
+                        days.append(days_str[index])
+
+            for day in days:
+                times.append(
+                    ClassTime(start_time=start_time, end_time=end_time, day=day)
+                )
+
+        if len(code) == 1:
+            lectures.append(Lecture(code=code, times=times))
+        if len(code) == 2:
+            subsection = SubSection(
+                code=code, times=times, type=i["activityOfferingType"]
+            )
+            for lecture in lectures:
+                if lecture.code == subsection.code[0]:
+                    lecture.add_subsection(subsection)
+
+    # print(lectures)
+
+
+if __name__ == "__main__":
+    get_course("PHYS 121")
